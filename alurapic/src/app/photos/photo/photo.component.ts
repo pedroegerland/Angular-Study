469cfa87a800e8d2dd5f96752980417885a47a70
pedroegerland <pedro.egerland94@gmail.com>
2020-03-10 23:44:46 -0300

    test

--- alurapic/src/app/photos/photo/photo.component.ts
+++ alurapic/src/app/photos/photo/photo.component.ts
@@ -1,12 +1,12 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'ap-photo',
-    templateUrl:'photo.component.html'
+    templateUrl: 'photo.component.html'
 })
-export class PhotoComponent{
-
+export class PhotoComponent {
+    
     @Input() description='';
-
+    
     @Input() url='';
 }
