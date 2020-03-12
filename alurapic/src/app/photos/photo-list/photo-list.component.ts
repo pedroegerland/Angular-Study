740635f8e45d4393d82fce90bdbd46fd84470fcc
pedroegerland <pedro.egerland94@gmail.com>
2020-03-12 00:36:56 -0300

    Fundamentos Angular Módulo 6

--- alurapic/src/app/photos/photo-list/photo-list.component.ts
+++ alurapic/src/app/photos/photo-list/photo-list.component.ts
@@ -1,32 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { debounceTime, } from 'rxjs/operators';
 
-import { PhotoService } from '../photo/photo.service';
 import { Photo } from '../photo/photo';
-
 @Component({
   selector: 'app-photo-list',
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit, OnDestroy {
 
   photos: Photo[] = []; 
+  filter: string = '';
+  debounce: Subject<string> = new Subject<string>();
   
-  constructor(
-    private photoService: PhotoService,
-    private activatedRoute: ActivatedRoute
-  ) { }
-
+  constructor(private activatedRoute: ActivatedRoute) { }
+  
   ngOnInit(): void {
-    
-      const userName = this.activatedRoute
-        .snapshot
-        .params
-        .userName;
+    this.photos = this.activatedRoute.snapshot.data.photos;
+    this.debounce
+    .pipe(debounceTime(300))
+    .subscribe(filter => this.filter = filter);
+  }
 
-      this.photoService
-        .listFromUser(userName)
-        .subscribe(photos => this.photos = photos);
+  ngOnDestroy(): void {
+    this.debounce.unsubscribe();
   }
 }
